bae6de749a47690646808c88ac8b876506ef0303
alaa <allafirst1@gmail.com>
2023-03-24 23:06:39 +0300

    changed the original by adding tailwind

--- react-form/src/App.js
+++ react-form/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Form from './components/Form'; 
+ 
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
